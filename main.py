8c2324c9c477d227ba35aa1977785818846a0cc6
vladGriguta <griguta_vlad_marius@yaho0.com>
2019-02-22 20:06:37 +0000

    readF

--- main.py
+++ main.py
@@ -1,8 +1,13 @@
+import numpy as np
+import pandas as pd
 
 
 
+def readFile(file='a_example.in'):
+    names = ['xStart','yStart','xFinish','yFinish','earliestStart','latestFinish']
+    df = pd.read_csv(file,skiprows=[0],header=None,names=names,delimiter=' ')
+    return df
 
 
-
-
-if __name__ == main:
+if __name__ == "__main__":
+    print('Functions imported   ')
